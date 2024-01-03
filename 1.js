26369aba5603ddc96a17e6aaf9939d508d4a13fd
Priyanka Soni <140409655+priyankasoni930@users.noreply.github.com>
2024-01-03 18:51:50 +0530

    Update 1.js

--- 1.js
+++ 1.js
@@ -49,3 +49,14 @@ year = 1991;
 console.log(typeof year);
 
 console.log(typeof null);
+
+// let, const and var
+let age = 30;
+age = 31;
+
+const birthYear = 1991;
+// birthYear = 1990;
+// const job;
+
+var job = 'programmer';
+job = 'teacher'
